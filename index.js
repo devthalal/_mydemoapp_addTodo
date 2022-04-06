2c1f16bf8f65ea0c30d64cde2a1ed10d8af34e0f
devthalal <thalal.hassan@neoito.com>
2022-04-06 14:50:02 +0530

    test

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
+/**
+  @param {} req
+  @param {} res
+ */
+const _mydemoapp_addTodo = async (req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE",
+    "Content-Type": "application/json",
+  };
 
-    import fs from 'fs'
-    import path from 'path'
-// For testing take pull from Appblox/node-blox-sdk and npm install from path
-// import { env } from 'node-blox-sdk'
-// env.init()
+  res.writeHead(200, headers);
+  res.write("Hello World, I am _mydemoapp_addTodo");
+  res.end();
+};
 
-const addTodo = async (req, res) => {
-  try {
-    // const DB_FILE = path.resolve(process.env.DB_FILE_PATH)
-    // const inmemDB = getDB(DB_FILE)
-    // const newId = new Date().getTime()
-    // const newItem = await getBody(req)
-    // const newEntry = { id: newId, item: newItem }
-    // console.log('Request to add -', newItem)
-    // inmemDB.push(newEntry)
-    // fs.writeFileSync(DB_FILE, JSON.stringify(inmemDB))
-    // console.log('Updated DB:
-', inmemDB)
-    // console.log('
-')
-    sendResponse(res, 200, "Hello from addTodo")
-  } catch (e) {
-    console.log(e)
-    sendResponse(res, 500, { status: 'failed', errMsg: e.message })
-  }
-}
+export default { _mydemoapp_addTodo };
